57e9ed62ce4fc7eedbdb963d7c1ca75a45178d83
666security <oskar.stawikowski1337@gmail.com>
2022-09-28 15:49:42 +0200

    php

--- rts.sql
+++ rts.sql
@@ -1,6 +1,6 @@
-insert into players (name,email,password) values ("pawel","pawel@gmail.com","123");
-insert into players (name,email,password) values ("kacper","kacper@gmail.com","1234");
-insert into players (name,email,password) values ("kamil","kamil@gmail.com","kamil123");
+insert into villagers (nick,email,password) values ("pawel","pawel@gmail.com","123");
+insert into villagers (nick,email,password) values ("kacper","kacper@gmail.com","1234");
+insert into villagers (nick,email,password) values ("kamil","kamil@gmail.com","kamil123");
 insert into resources (name) values ("drewno","glina","zelazo");
 insert into buildings (name) values ("dol z gliny","huta zelaza","huta drwala");
 insert into troops (name) values ("lucznik","rycerz","miecznik");
